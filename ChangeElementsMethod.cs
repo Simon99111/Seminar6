fcf32fbc45c3a82ec8e4a37c6b8d7e6136989d82
Семён Курочкин <81917221+Simon99111@users.noreply.github.com>
2023-02-26 15:49:37 +0300

    Delete ChangeElementsMethod.cs

--- ChangeElementsMethod.cs
+++
@@ -1,9 +0,0 @@
-int ChangeElements(int[] array) // Замена положительных элементов массива на отрицательные и наоборот
-{
-    for(int i = 0; i < array.Length; i++)
-    {
-        array[i] *= -1;
-    }
-    return array;
-}
-
